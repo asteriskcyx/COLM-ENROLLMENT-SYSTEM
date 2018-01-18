b9ccff34d73fc4280e854e4dea2940a1a79f9ff6
asteriskcyx <asteriskcyx@gmail.com>
2018-01-18 11:49:46 +0800

    Fifth Commit

---
+++ COLM ENROLLMENT SYSTEM/COLM ENROLLMENT SYSTEM/SQL Commands and Functions/SQL Functions.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Xml
+Imports System.Xml.Linq
+Module SQL_Functions
+
+    Public Function VerifyDatabaseFunctions(FunctionName As String) As Boolean
+        Dim FunctionExists As Boolean = False
+        Using conn As New SqlConnection(StringConnection)
+            conn.Open()
+            Using comm As New SqlCommand("SELECT * FROM dbo.sysobjects WHERE name = @functionname AND type = 'IF'", conn)
+                comm.Parameters.AddWithValue("@functionname", FunctionName)
+                Using reader As SqlDataReader = comm.ExecuteReader
+                    If reader.HasRows = True Then
+                        FunctionExists = True
+                    End If
+                End Using
+            End Using
+        End Using
+        Return FunctionExists
+    End Function
+
+    Public Sub FN_FunctionName()
+
+        Dim FunctionCodes As String = <a>
+
+                                      </a>
+        Using conn As New SqlConnection(StringConnection)
+            conn.Open()
+            Using comm As New SqlCommand(FunctionCodes, conn)
+                comm.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+End Module
